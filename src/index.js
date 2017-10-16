c5c91d6118a65c875b07e1523fa1c4736ff79d42
ayubov <alvi.ayubov@yandex.ru>
2017-10-16 16:05:26 +0300

    add info

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-export default () => console.log('test');
+import commander from 'commander';
+
+export default () => {
+  commander
+    .version('0.0.1')
+    .arguments('<firstConfig> <secondConfig>')
+    .option('-f, --format [type]', 'output format')
+    .description('Compares two configuration files and shows a difference.')
+    .parse(process.argv);
+};
