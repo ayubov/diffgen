5b83f8be19dacc187f7dc56c4db34082446ed270
ayubov <alvi.ayubov@yandex.ru>
2017-10-19 16:23:22 +0300

    move parsers to global scope

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ import path from 'path';
 import ini from 'ini';
 import yaml from 'js-yaml';
 
+const parsers = {
+  '.json': JSON.parse,
+  '.yml': yaml.safeLoad,
+  '.ini': ini.parse,
+};
+
 export default (firstConfigPath, secondConfigPath) => {
   const firstConfig = fs.readFileSync(firstConfigPath, 'utf8');
   const secondConfig = fs.readFileSync(secondConfigPath, 'utf8');
   const format = path.extname(firstConfigPath);
-  const parse = {
-    '.json': JSON.parse,
-    '.yml': yaml.safeLoad,
-    '.ini': ini.parse,
-  };
-
-  const firstConfigContent = parse[format](firstConfig);
-  const secondConfigContent = parse[format](secondConfig);
+  const firstConfigContent = parsers[format](firstConfig);
+  const secondConfigContent = parsers[format](secondConfig);
 
   const unionKeys = _.union(Object.keys(firstConfigContent), Object.keys(secondConfigContent));
   const totalDiffObj = unionKeys.reduce((acc, key) => {
