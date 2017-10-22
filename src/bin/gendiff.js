80aca98652a7d592d5d0140acbecc5016a8ff099
ayubov <alvi.ayubov@yandex.ru>
2017-10-22 11:42:36 +0300

    rename function

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
 import commander from 'commander';
-import jsonDiff from '..';
+import genDiff from '..';
 
 commander
   .version('0.0.1')
   .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'output format')
   .description('Compares two configuration files and shows a difference.')
-  .action((firstConfig, secondConfig) => console.log(jsonDiff(firstConfig, secondConfig)))
+  .action((firstConfig, secondConfig) => console.log(genDiff(firstConfig, secondConfig)))
   .parse(process.argv);
